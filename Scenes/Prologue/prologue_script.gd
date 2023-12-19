2a389c4a1067cd49520b7136a2504381b65cab58
zadzaza <maxawilow@gmail.com>
2023-12-19 15:56:39 +0400

    git add

--- Scenes/Prologue/prologue_script.gd
+++ Scenes/Prologue/prologue_script.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func _process(delta):
 	if Input.is_action_pressed("piska_1") and Input.is_action_pressed("piska_2") and Input.is_action_pressed("piska_3"):
-		$PlayerScene/PointLight2D.enabled = true
-	else: $PlayerScene/PointLight2D.enabled = false
+		$PlayerScene/Prizrak.enabled = true
+	else: $PlayerScene/Prizrak.enabled = false
