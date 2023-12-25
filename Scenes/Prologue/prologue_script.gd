8f25a8b8b3036703b9ef1467413b02f74d5311cc
zadzaza <maxawilow@gmail.com>
2023-12-25 13:11:25 +0300

    git add

--- Scenes/Prologue/prologue_script.gd
+++ Scenes/Prologue/prologue_script.gd
@@ -4,3 +4,7 @@ func _process(delta):
 	if Input.is_action_pressed("piska_1") and Input.is_action_pressed("piska_2") and Input.is_action_pressed("piska_3"):
 		$PlayerScene/Prizrak.enabled = true
 	else: $PlayerScene/Prizrak.enabled = false
+	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/prologue.dialogue"), "start")
