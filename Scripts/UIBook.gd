71e09cf211a3f3255233a1d5f9df76884a1fc9d7
zadzaza <maxawilow@gmail.com>
2024-01-13 23:19:52 +0300

    git add

--- Scripts/UIBook.gd
+++ Scripts/UIBook.gd
@@ -1,59 +1,57 @@
 extends CanvasLayer
 
-var z_p_selected = false
-var z_i_selected = false
-var z_s_selected = false
+@export var p_selected = false
+@export var i_selected = false
+@export var s_selected = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	%AnimationPlayer.play("appearance")
-	
+
 func _process(delta):
-	if z_p_selected:
-		%ZakladkaPeoples.set_animation("selected")
-		z_i_selected = false
-		z_s_selected = false
-	elif z_i_selected:
-		%ZakladkaInv.set_animation("selected")
-		z_p_selected = false
-		z_s_selected = false
-	elif z_s_selected:
-		%ZakladkaSettings.set_animation("selected")
-		z_i_selected = false
-		z_p_selected = false
+	if p_selected:
+		%Peoples.set_texture_normal(load("res://Sprites/UI/UIBook/peoples_selected.png"))
+	elif !p_selected:
+		%Peoples.set_texture_normal(load("res://Sprites/UI/UIBook/peoples_none.png"))
 	
-	if !z_p_selected:
-		%ZakladkaPeoples.set_animation("none")
-	if !z_i_selected:
-		%ZakladkaInv.set_animation("none")
-	if !z_s_selected:
-		%ZakladkaSettings.set_animation("none")
+	if i_selected:
+		%Inv.set_texture_normal(load("res://Sprites/UI/UIBook/inv_selected.png"))
+	elif !i_selected:
+		%Inv.set_texture_normal(load("res://Sprites/UI/UIBook/inv_none.png"))
+	
+	if s_selected:
+		%Settings.set_texture_normal(load("res://Sprites/UI/UIBook/settings_selected.png"))
+	elif !s_selected:
+		%Settings.set_texture_normal(load("res://Sprites/UI/UIBook/settings_none.png"))
+
 
 func _on_animation_player_animation_finished(anim_name):
 	%Book.set_animation("book_open")
-	%Zakladki.set_visible(true)
+	%Elements.set_visible(true)
 
 
-func _on_zakladka_peoples_area_2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			z_p_selected = true
-			print("z_p_selected ", z_p_selected)
-			print("z_i_selected ", z_i_selected)
-			print("z_s_selected ", z_s_selected)
+func _on_peoples_pressed():
+	p_selected = true
+	i_selected = false
+	s_selected = false
+	
+	%Page1.set_visible(true)
+	%Page2.set_visible(false)
 
-func _on_zakladka_inv_area_2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			z_i_selected = true
-			print("z_i_selected ", z_i_selected)
-			print("z_p_selected ", z_p_selected)
-			print("z_s_selected ",z_s_selected)
+func _on_inv_pressed():
+	i_selected = true
+	p_selected = false
+	s_selected = false
+	
+	%Page1.set_visible(false)
+	%Page2.set_visible(true)
 
-func _on_zakladka_settings_area_2d_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			z_s_selected = true
-			print("z_s_selected ",z_s_selected)
-			print("z_p_selected ", z_p_selected)
-			print("z_i_selected ", z_i_selected)
+
+func _on_settings_pressed():
+	s_selected = true
+	i_selected = false
+	p_selected = false
+	
+	%Page1.set_visible(false)
+	%Page2.set_visible(false)
+
