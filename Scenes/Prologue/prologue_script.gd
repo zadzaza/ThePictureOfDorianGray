78977ac6139e18a066fd89a5e6f8d11d63b67052
zadzaza <maxawilow@gmail.com>
2024-02-17 05:26:24 +0300

    git add

--- Scenes/Prologue/prologue_script.gd
+++ Scenes/Prologue/prologue_script.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-var material1 = load("res://Scenes/Prologue/shaders/water_reflection.gdshader")
 @onready var path_follow = $Path2D/PathFollow2D
-var go_to_path = false
+@onready var camera_pl = $Path2D/PathFollow2D/PlayerScene/PlCamera
+@onready var camera_world = $WorldCamera
+@onready var player = $Path2D/PathFollow2D/PlayerScene
+
+var speed = 0.0
 
 func _ready():
+	camera_world.make_current()
+	
 	$MainCanvasLayer/Transition.set_visible(true)
 	$AnimationTree/TransitionAnimation.play("light_up")
 	await $AnimationTree/TransitionAnimation.animation_finished
 	$MainCanvasLayer/Transition.set_visible(false)
-	go_to_path = true
+	speed = 0.1
 	#DialogueManager.show_example_dialogue_balloon(load("res://dialogue/prologue.dialogue"), "start")
 
 func _input(event):
@@ -17,5 +22,7 @@ func _input(event):
 		%UIBookMenu.show()
 
 func _process(delta):
-	if go_to_path:
-		path_follow.progress_ratio += delta * 0.1
+	path_follow.progress_ratio += delta * speed
+	if path_follow.progress_ratio == 1.0:
+		camera_pl.make_current()
+
